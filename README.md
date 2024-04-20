0e51897ab9670a17421ff55c1f2c47641f44e7dd
Erick <erick.2210478300057@faeterj-rio.edu.br>
2024-04-20 15:22:48 -0300

    Estrutura de arquivos e home page

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # eco-escambo
 Repositório para armazenar o trabalho da disciplina de 3DAW.
+
+Divisão de Paginas
+Gaby: Tela de login , cadastro de usuário, cadastro de produto, tela de verificação e headers e footers;
+Erick: Tela de meus produtos, ofertas, analise de ofertas e index com carrossel;
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eco Escambo</title>
+    <link rel="shortcut icon" href="/src/img/header/logo-eco-escambo.jpg">
+    <link rel="stylesheet" href="/src/css/style.css">
+    <script src="./src//js//carousel.js" defer></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
+</head>
+
+<body>
+    <?php require_once("./src/pages/header/header.html"); ?>
+    <div class="container">
+        <div class="banner">
+            <img src="/src//img//banner.png">
+        </div>
+        <div class="slider-titulo">
+            <h2>Sugestões que podem te interessar</h2>
+        </div>
+        <div class="container-body">
+            <div class="slider-container">
+                <div class="slider-box">
+                    <button id="prev-slide" class="slide-button material-symbols-rounded">
+                        chevron_left
+                    </button>
+                    <ul class="image-list">
+                        <img src="/src//img//header//logo-eco-escambo.jpg" alt="img-1" class="image-item">
+                        <img src="/src//img//header//logo-eco-escambo.jpg" alt="img-2" class="image-item">
+                        <img src="/src//img//header//logo-eco-escambo.jpg" alt="img-3" class="image-item">
+                        <img src="/src//img//header//logo-eco-escambo.jpg" alt="img-4" class="image-item">
+                        <img src="/src//img//header//logo-eco-escambo.jpg" alt="img-5" class="image-item">
+                        <img src="/src//img//header//logo-eco-escambo.jpg" alt="img-6" class="image-item">
+                        <img src="/src//img//header//logo-eco-escambo.jpg" alt="img-7" class="image-item">
+                        <img src="/src//img//header//logo-eco-escambo.jpg" alt="img-8" class="image-item">
+                        <img src="/src//img//header//logo-eco-escambo.jpg" alt="img-9" class="image-item">
+                    </ul>
+                    <button id="next-slide" class="slide-button material-symbols-rounded">
+                        chevron_right
+                    </button>
+                </div>
+                <div class="slider-scrollbar">
+                    <div class="scrollbar-track">
+                        <div class="scrollbar-thumb"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
