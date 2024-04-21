ec13d7d2dac77d354b15af471df15472987573e0
Gabrielly Oliveira <oliveiragabrielly.dev@gmail.com>
2024-04-20 23:50:50 -0300

    Ajuste de detalhes

--- login.php
+++ login.php
@@ -14,20 +14,26 @@
   <?php require_once("./src/pages/header/header.php"); ?>
 
   <div class="container--login-info">
-    <form method="post" action="valida-user.php">
-      <label for="usuario">Usuário:</label>
-      <input type="text" id="usuario" name="username">
-      <label for="senha">Senha:</label>
-      <input type="password" id="senha" name="password">
-      <input type="submit" value="Entrar">
-    </form>
+    <div class="container--form">
+      <?php
+        if(isset($_GET["erro"])){
+          echo "<p>Senha e/ou usuário incorreto(s).</p>";
+        }
+      ?>
+      <form method="post" action="valida-user.php">
+        <label for="usuario">Usuário</label>
+        <br>
+        <input type="text" id="usuario" name="username" placeholder="Digite seu usuário">
+        <br>
+        <label for="senha">Senha</label>
+        <br>
+        <input type="password" id="senha" name="password" placeholder="Digite sua senha">
+        <br>
+        <input type="submit" value="Entrar">
+      </form>
+      <div><a href="esqueci-minha-senha.php">Esqueci a minha senha</a> | <a href="">Esqueci o meu usuário</a></div>
+    </div>
   </div>
-
-  <?php
-      if(isset($_GET["erro"])){
-        echo "<p>Senha e/ou usuário incorreto(s).</p>";
-      }
-    ?>
 
   <?php require_once("./src/pages/footer/footer.html"); ?>
 </body>
